483d73f439135a3a6b126a3c1d857bb19c9fdc2b
smart0815 <sandy815.dev@outlook.com>
2021-10-22 17:34:16 +0800

    Add: index file

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const EmployeeOrgApp_1 = require("./EmployeeOrgApp");
+const app = new EmployeeOrgApp_1.default();
+app.move(1, 4);
+//# sourceMappingURL=index.js.map
